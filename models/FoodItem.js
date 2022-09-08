374021b0531ae08ba4065c4644a80e76c4082aaf
Anthony Brown <ac524.brown@gmail.com>
2022-09-07 18:23:18 -0700

    Created association between user and food item.

--- models/FoodItem.js
+++ models/FoodItem.js
@@ -29,4 +29,4 @@ FoodItem.init(
   }
 );
 
-module.exports = User;
+module.exports = FoodItem;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require('./User');
+const FoodItem = require('./FoodItem');
 
 // Create associations
+User.hasMany(FoodItem, {
+  onDelete: 'CASCADE',
+  foreignKey: 'id',
+});
 
-module.exports = { User };
+FoodItem.belongsTo(FoodItem, {
+  foreignKey: 'id'
+});
+
+module.exports = { User, FoodItem };
